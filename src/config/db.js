77ce517a090148d35dbb769a3377f6e74e4b68f7
Nindadela_ <nindadela07@gmail.com>
2025-10-31 14:35:25 +0700

    Update DB connection & fix login/register for Railway

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,8 @@
-const { Pool } = require('pg'); 
+const { Pool } = require('pg');
 
-const db = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,  // DATABASE_URL Railway
+  ssl: { rejectUnauthorized: false }          // penting di Railway
 });
 
-module.exports = db;
+module.exports = pool;
